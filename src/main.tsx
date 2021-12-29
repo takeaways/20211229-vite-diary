01457b5abaab40c46481d7a3953fc7f552b62c27
JangGeonil <wkdrjsdlf2@gmail.com>
2021-12-29 20:57:38 +0900

    chore including LAN and public addresses

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById("root"),
 );
